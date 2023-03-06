4d132fd8661f5521033dceb7ad118c3a47d57f3e
javicia <javier.garcia.perez@hotmail.com>
2023-03-06 15:19:56 +0100

    drawer

--- app/src/main/java/com/example/passlive/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/example/passlive/presentation/home/HomeFragment.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.example.passlive.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeFragment : Fragment() {
+class HomeFragment: Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
@@ -24,12 +25,8 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
-    }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
 
-        binding.tvHome.text = "Bienvenido/a: ${args.uid}"
     }
 
     override fun onDestroyView() {
