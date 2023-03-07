d148cf9e8c137f23ee7e34bd637aa9c9fd698bb0
javicia <javier.garcia.perez@hotmail.com>
2023-03-07 13:56:04 +0100

    update

--- app/src/main/java/com/example/passlive/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/example/passlive/presentation/home/HomeFragment.kt
@@ -2,19 +2,28 @@ package com.example.passlive.presentation.home
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.R
 import androidx.navigation.fragment.navArgs
 import com.example.passlive.databinding.FragmentHomeBinding
+import com.google.android.material.navigation.NavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class  HomeFragment: Fragment() {
+class  HomeFragment: Fragment(){
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var drawer:DrawerLayout
+    private lateinit var toogle: ActionBarDrawerToggle
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +32,10 @@ class  HomeFragment: Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
     }
 
     override fun onDestroyView() {
